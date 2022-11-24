f43ca16dffde8f783a7cf8c6fd5da9202287d459
Varun Kumar <57076268+varkum@users.noreply.github.com>
2022-11-24 13:41:28 -0500

    initial

---
+++ lib/spotify/api/wrapper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "wrapper/version"
+
+module Spotify
+  module Api
+    module Wrapper
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
